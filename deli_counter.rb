e2ff4ca40063e768704b0dbb4d9fb6447b81be61
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-09 20:52:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,9 @@ def line(array)
     puts "The line is currently:" + current_line
   end
 end
+
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.index(name) + 1} in line."
+
+end
