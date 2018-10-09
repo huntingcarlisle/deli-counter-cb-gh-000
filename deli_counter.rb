6e66f01590a1946ea24848840f66251a36d93d80
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-09 20:42:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,11 @@
 # Write your code here.
 def line(array)
-    "The line is currently empty." if array.length < 1
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    print "The line is currently:"
+    array.each do |customer|
+      print " #{array.index[customer]}. #{customer}"
+      print "\n""
+  end
 end
